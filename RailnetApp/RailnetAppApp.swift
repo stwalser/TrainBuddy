abdb1d232f8b4ea9be78437c17b8c990f525578f
Stefan <65011720+Bumblebee118@users.noreply.github.com>
2023-12-02 11:55:12 +0100

    Initial Commit

---
+++ RailnetApp/RailnetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RailnetAppApp.swift
+//  RailnetApp
+//
+//  Created by Stefan Walser on 02.12.23.
+//
+
+import SwiftUI
+
+@main
+struct RailnetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
